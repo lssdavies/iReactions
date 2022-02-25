a59562e9f550dc6b07c23f004e4ff52a42febc06
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2022-02-24 21:44:30 -0800

    added index file to store created models in one location

---
+++ models/index.js
@@ -0,0 +1,6 @@
+//Import the models into this file, it will package up all the models
+const User = require("./User");
+const Thoughts = require("./Thoughts");
+
+module.exports = { User, Thoughts };
+//this will be imported in ./controllers/
